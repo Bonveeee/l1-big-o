3623341737308bf3048b17ec7ac558e0e407bde5
bonveeee <bonochieng@gmail.com>
2022-06-15 13:31:17 +0300

    to be solve bug logarithmic time part

--- index.js
+++ index.js
@@ -2,34 +2,66 @@
 const print = (value) => console.log(value)
 
 // user scores
-const scores = [99 ,80, 77, 86, 50, 33, 76, 90, 54, 66]
+const scores = [99, 80, 77, 86, 50, 33, 76, 90, 54, 66]
 
 // TODO: IDENTIFY THE TIME COMPLEXITY OF THE FOLLOWING ARRAY METHODS
 // 1: PUSH
-scores.push(25)
+//scores.push(25)
+// it is constant time
 
 // 2: POP
-scores.pop()
+//scores.pop()
+// it is constant time
 
 // 3: UNSHIFT
-scores.unshift(88)
+//scores.unshift(88)
+
+// it is linear time 
 
 // 4: forEach
-scores.forEach((element) => {print(element)})
-
+//scores.forEach((element) => {
+//print(element)
+//})
+// it is linear time 
 
 // TODO: CREATE A SIMPLE SEARCH ALGORITHM THAT USES LINEAR TIME COMPLEXITY
-const findValueLinear = (value) => {
-    // return the index of the value if present, return -1 if absent
-}
-// print(`190 is at index: ${findValueLinear(190)}`)
-// print(`76 is at index: ${findValueLinear(76)}`)
+// const findValueLinear = (value) => {
+//     // return the index of the value if present, return -1 if absent
+//     let index = [];
+//     for (let i = 0; i < scores.length; i++) {
+//         const score = scores[i]
+//         if (score === value) {
+//             index.push(i);
+//             return index
+//         }
+//     }
+//     return index
+// }
+// //print(`190 is at index: ${findValueLinear(190)}`)
+//print(`76 is at index: ${findValueLinear(76)}`)
 
 
 // TODO: CREATE A SIMPLE SEARCH ALGORITHM THAT USES LOGARITHMIC TIME COMPLEXITY
-const findValueLog = (value) => {
+
+scores.sort()
+const findValueLog = (value, top, bottom) => {
+    let index = -1;
+    const halfops = (top + bottom) / 2;
+    const half = parseInt(halfops);
+    const valBefore = scores[half - 1];
+    if (value < valBefore) {
+        //tophalf
+        findValueLog(value, half - 1, 0);
+    } else {
+        if (value === scores[half]) {
+            return half
+        }
+        findValueLog(value, scores.length - 1, 0);
+    }
+
+    return index
+
     // return the index of the value if present, return -1 if absent
 }
-
-// print(`90 is at index: ${findValueLog(90)}`)
-// print(`88 is at index: ${findValueLog(88)}`)
+print(`90 is at index: ${findValueLog(90, scores.length -1 , 0 )}`)
+print(`33 is at index: ${findValueLog(33, scores.length -1 , 0)}`)
